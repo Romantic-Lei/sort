dc9bb8bbd8dfd1262d872f9946f83e6669c25564
Memories6 <1371910434@qq.com>
2020-02-02 14:59:17 +0800

    选择排序

--- SelectSort/selectsort.go
+++ SelectSort/selectsort.go
@@ -11,17 +11,17 @@ import (
 */
 func SelectSort(arr *[5]int) {
 	// 1. 先完成第一个最大值和 arr[0] 交换
+	// 外层循环，控制比较轮数
 	for i := 0; i < len(arr) - 1; i++ {
-		max := arr[i]
-		maxIndex := i
+		maxIndex := i // 假设当前值为最大值，保存其的索引
 		// 遍历后面的进行比较
 		for j := i + 1; j < len(arr); j++ {
-			if max < arr[j] {
-				max = arr[j]
+			// 内层循环，控制每轮比较次数
+			if arr[maxIndex] < arr[j] {
 				maxIndex = j
 			}
 		}
-		// 交换
+		// 如果假设的最大值索引和比较之后的索引值不一致则需要交换
 		if maxIndex != i {
 			arr[i], arr[maxIndex] = arr[maxIndex], arr[i]
 		}
